63938635f1136b77884e77c368ae153a04014451
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2023-07-30 14:24:36 -0300

    Jane_vazia
    Criei um janela vazia do jogo space invaders 2.0. Criei uma classe AlienInvasion que vai gerenciar todo o jogo.

---
+++ alien_invasion.py
@@ -0,0 +1,23 @@
+import sys
+import pygame
+
+
+class AlienInvasion():
+    """Um jogo curto sobre invasao alien. Bem parecido com space invaders!"""
+    def __init__(self) -> None:
+        """Inicializa as variáveis e o jogo"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode(1200, 800)
+        pygame.display.set_caption("Space invaders 2.0")
+
+        def run_game(self):
+            """Inicializa o loop do jogo"""
+            while True:
+                # Observa eventos do teclado
+                for event in pygame.event.get():
+                    if event == pygame.QUIT:
+                        sys.exit()
+                
+                # Deixa a tela desenhada mais recente visível
+                pygame.display.flip()
