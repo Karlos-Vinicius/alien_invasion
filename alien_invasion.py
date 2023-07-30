0821af263f9279e666030905759ffb67579d3e1f
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2023-07-30 14:32:26 -0300

    Remoçao da identaçao
    Foi necessário remover a identaçao do método rung_game para ser retirado do escopo do método __init__

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,19 +8,19 @@ class AlienInvasion():
         """Inicializa as variáveis e o jogo"""
         pygame.init()
 
-        self.screen = pygame.display.set_mode(1200, 800)
+        self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Space invaders 2.0")
 
-        def run_game(self):
-            """Inicializa o loop do jogo"""
-            while True:
-                # Observa eventos do teclado
-                for event in pygame.event.get():
-                    if event == pygame.QUIT:
-                        sys.exit()
-                
-                # Deixa a tela desenhada mais recente visível
-                pygame.display.flip()
+    def run_game(self):
+        """Inicializa o loop do jogo"""
+        while True:
+            # Observa eventos do teclado
+            for event in pygame.event.get():
+                if event == pygame.QUIT:
+                    sys.exit()
+            
+            # Deixa a tela desenhada mais recente visível
+            pygame.display.flip()
 
 if __name__ == '__main__':
     # Cria uma instância do jogo e o executa
