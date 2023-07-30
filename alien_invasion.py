ebedaaf9b16ab8f2f34657efe10093a4349f4f13
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2023-07-30 14:45:56 -0300

    Add_frame_fill
    Adicionando preenchimento e a taxa de quadros por segundo

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,8 @@ class AlienInvasion():
     def __init__(self) -> None:
         """Inicializa as variáveis e o jogo"""
         pygame.init()
+        self.clock = pygame.time.Clock()
+        self.bg_color = (230, 230, 230)
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Space invaders 2.0")
@@ -18,8 +20,12 @@ class AlienInvasion():
             for event in pygame.event.get():
                 if event == pygame.QUIT:
                     sys.exit()
+
+            # Redesenha a tela a cada passagem pelo loop
+            self.screen.fill(self.bg_color)
             
             # Deixa a tela desenhada mais recente visível
+            self.clock.tick(60)
             pygame.display.flip()
 
 if __name__ == '__main__':
