c8cc335523516391088baea5af069cc7095c1527
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2023-07-31 10:15:16 -0300

    Ship image
    Atualizando as docstring. Criaçao da pasta imagens para conter uma imagem com a exterçao .bmp

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,13 +8,13 @@ from settings import Settings
 class AlienInvasion():
     """Um jogo curto sobre invasao alien. Bem parecido com space invaders!"""
     def __init__(self) -> None:
-        """Inicializa as variáveis e o jogo"""
+        """Classe geral que gerencia o comportamento do jogo"""
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode((self.settings.screen_height, self.settings.screen_height))
-        pygame.display.set_caption("Space invaders 2.0")
+        pygame.display.set_caption("Space Invaders 2.0")
 
     def run_game(self):
         """Inicializa o loop do jogo"""
