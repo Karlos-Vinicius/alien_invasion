62d2692e5124533166871ce9918d3ade5ce1cc7e
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2023-08-01 11:32:11 -0300

    Error trying to the exit game
    Concertei o erro para, assim que eu apertar no ícone da minha janela em formato de 'X', sair do jogo

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,6 +14,8 @@ class AlienInvasion():
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
+        self.running_game = True
+
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Space Invaders 2.0")
 
@@ -21,11 +23,11 @@ class AlienInvasion():
 
     def run_game(self):
         """Inicializa o loop do jogo"""
-        while True:
+        while self.running_game:
             # Observa eventos do teclado
             for event in pygame.event.get():
-                if event == pygame.QUIT:
-                    sys.exit()
+                if event.type == pygame.QUIT:
+                    self.running_game = False
 
             # Redesenha a tela a cada passagem pelo loop
             self.screen.fill(self.settings.bg_color)
@@ -34,6 +36,9 @@ class AlienInvasion():
             # Deixa a tela desenhada mais recente visível
             self.clock.tick(60)
             pygame.display.flip()
+        
+        # Fecha o jogo
+        sys.exit()
 
 if __name__ == '__main__':
     # Cria uma instância do jogo e o executa
