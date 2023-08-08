14eefc366292e7a239785bbffe12125bce8c8b04
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2023-08-08 11:05:10 -0300

    Size window
    Mudando o tamanho da janela para 1280x720

--- settings.py
+++ settings.py
@@ -3,8 +3,8 @@ class Settings:
     def __init__(self) -> None:
         """Configuraçoes iniciais do jogo"""
         # Configuraçoes de tela
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1280
+        self.screen_height = 720
         self.bg_color = (230, 230, 230)
 
         # Configuraçoes da espaçonave
