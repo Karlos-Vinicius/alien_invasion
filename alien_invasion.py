7dd6adf35e7ebc77d1518f29dd39cf580e6aa369
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2023-08-13 19:09:20 -0300

    Full screen
    Deixando o jogo em tela cheia utilizando o pygame.FULLSCREEN

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,9 +14,13 @@ class AlienInvasion():
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
+
+
         self.running_game = True
 
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_height = self.screen.get_height()
+        self.settings.screen_width = self.screen.get_width()
         pygame.display.set_caption("Space Invaders 2.0")
 
         self.ship = Ship(self)
