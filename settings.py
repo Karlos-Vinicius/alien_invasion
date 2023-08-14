34f36d978e806d9cddf7d27c121e9435488d78fe
Karlos Vinícius Carvalho de Olivera <140765802+Karlos-Vinicius@users.noreply.github.com>
2023-08-13 22:57:54 -0300

    settings bullet
    Adicionando as configuraçoes do projétil

--- settings.py
+++ settings.py
@@ -9,3 +9,9 @@ class Settings:
 
         # Configuraçoes da espaçonave
         self.ship_speed = 1.5
+
+        # Configuraçoes do projétil
+        self.bullet_speed = 1.5
+        self.bullet_widht = 5
+        self.bullet_height = 14
+        self.bullet_bg_color = (60, 60, 60)
